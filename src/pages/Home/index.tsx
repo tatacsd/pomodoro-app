02937384956f1d7f25bdaf4a72b615f249a3edab
thayscasado <thayscasado@hotmail.com>
2022-11-20 12:38:29 -0800

    edits task input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,13 +19,28 @@ export function Home() {
             type="text"
             id="task"
             placeholder="What are you working on?"
+            list="tasks-suggestions"
           />
+
+          <datalist id="tasks-suggestions">
+            <option value="Front-end development" />
+            <option value="Back-end development" />
+            <option value="Mobile development" />
+            <option value="UI/UX design" />
+            <option value="Marketing" />
+            <option value="Writing" />
+            <option value="Reading" />
+            <option value="Learning" />
+          </datalist>
 
           <label htmlFor="minutsAmount">For how long?</label>
           <MinutesAmountInput
             type="number"
             id="minutsAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutes.</span>
         </FormContainer>
