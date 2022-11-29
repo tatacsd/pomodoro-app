9db6cb7de2612da4b65e81d8089a4057156e1ad0
thayscasado <thayscasado@hotmail.com>
2022-11-29 00:34:53 -0800

    adds reset to the form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,7 +27,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -40,6 +40,8 @@ export function Home() {
 
   const handleCreateNewCycle = (data: NewCycleFormData) => {
     console.log(data);
+
+    reset();
   };
 
   return (
