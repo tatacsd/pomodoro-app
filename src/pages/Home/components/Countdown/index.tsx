faac991f1370a238b9c08b93b127b2cec6ad1811
thayscasado <thayscasado@hotmail.com>
2022-11-29 03:35:07 -0800

    refactoring countdown using Context api

---
+++ src/pages/Home/components/Countdown/index.tsx
@@ -0,0 +1,76 @@
+import { differenceInSeconds } from 'date-fns';
+import { useContext, useEffect } from 'react';
+import { CycleContext } from '../..';
+import { CountdownContainer, Separator } from './styles';
+
+export function Countdown() {
+  const {
+    activeCycle,
+    setCycles,
+    activeCycleID,
+    secondsCountdown,
+    setSecondsCountdown,
+  } = useContext(CycleContext);
+
+  const totalActiveSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle
+    ? totalActiveSeconds - secondsCountdown
+    : 0;
+
+  const minutesLeft = String(Math.floor(currentSeconds / 60)).padStart(2, '0');
+  const secondsLeft = String(currentSeconds % 60).padStart(2, '0');
+
+  // countdown
+  useEffect(() => {
+    let interval: number;
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startedAt
+        );
+
+        if (secondsDifference >= totalActiveSeconds) {
+          // finish cycle
+          setCycles((state) =>
+            state.map((cycle) => {
+              if (cycle.id === activeCycleID) {
+                return {
+                  ...cycle,
+                  finishedAt: new Date(), // set finishedAt
+                };
+              }
+              return cycle;
+            })
+          );
+
+          setSecondsCountdown(totalActiveSeconds);
+          clearInterval(interval);
+        } else {
+          setSecondsCountdown(secondsDifference);
+        }
+      }, 1000);
+    }
+
+    // cleanup
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle, totalActiveSeconds, activeCycleID, setCycles]);
+
+  // Document title with active cycle task
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutesLeft}:${secondsLeft} - ${activeCycle.task}`;
+    }
+  }, [minutesLeft, secondsLeft]);
+  return (
+    <CountdownContainer>
+      <span>{minutesLeft[0]}</span>
+      <span>{minutesLeft[1]}</span>
+      <Separator>:</Separator>
+      <span>{secondsLeft[0]}</span>
+      <span>{secondsLeft[1]}</span>
+    </CountdownContainer>
+  );
+}
