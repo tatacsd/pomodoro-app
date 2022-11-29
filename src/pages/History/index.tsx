3b2d6880ac8b4224fc7dd693b13eb2f26e64b584
thayscasado <thayscasado@hotmail.com>
2022-11-28 23:25:45 -0800

    adds history page skeleton

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,84 @@
+import { HistoryContainer, HistoryList } from './styles';
+
 export function History() {
-  return <h1>History</h1>;
+  return (
+    <HistoryContainer>
+      <h1>History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>task</th>
+              <th>duration</th>
+              <th>start</th>
+              <th>status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+            <tr>
+              <td>task 1</td>
+              <td>1h</td>
+              <td>10:00</td>
+              <td>done</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
 }
---
+++ src/pages/History/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`;
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+
+    min-width: 600px;
+
+    th {
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem; /* 14px */
+      line-height: 1.6rem; /* 24px */
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1rem;
+      }
+
+      &:last-child {
+        border-bottom-left-radius: 8px;
+        padding-right: 1rem;
+      }
+    }
+
+    td {
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      background-color: ${(props) => props.theme['gray-700']};
+      padding: 1rem;
+      font-size: 0.875rem; /* 14px */
+      line-height: 1.6rem; /* 24px */
+
+      &:first-child {
+        width: 50%;
+        border-top-left-radius: 8px;
+      }
+
+      &:last-child {
+        border-bottom-left-radius: 8px;
+      }
+    }
+  }
+`;
